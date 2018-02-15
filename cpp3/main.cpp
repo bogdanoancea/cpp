c53c6997490d0e6f660a0bedf9985441c755be95
bogdanoancea <bogdan.oancea@gmail.com>
2018-02-15 13:56:59 +0200

    some new code addded

--- cpp3/main.cpp
+++ cpp3/main.cpp
@@ -1,14 +1,38 @@
 #include <iostream>
 #include<stdexcept>
+#include<list>
+
 using namespace std;
 
 constexpr double C = 299792.458;
 
-class Vector {
+
+class Container {
+public:
+    virtual double& operator[](int i) = 0;
+    virtual int size() const = 0;
+    virtual ~Container(){};
+};
+
+
+void use(Container& c) {
+    const int sz = c.size();
+    for(auto i=0; i!=sz;++i)
+        cout << c[i]<< endl ;
+}
+
+
+
+class Vector{
 public:
     Vector(int size); //: elem {new double[size]}, s{size} {}
+    Vector(std::initializer_list<double> lst):elem{new double[lst.size()]}, s{lst.size()}{
+        copy(lst.begin(), lst.end(), elem);
+    }
+    ~Vector() { delete[] elem;}
     double& operator [](int i);//{return elem[i];}
-    int size(){return s;}
+    int size()const {return s;}
+    void push_back(double d);
 private:
     int s;
     double *elem;
@@ -25,7 +49,55 @@ Vector::Vector(int size) {
     if(size <0)
         throw length_error{"negative size"};
     elem = new double[size];
+    for(auto i = 0; i != size; ++i)
+        elem[i] = 0;
     s = size;
+}
+
+
+class Vector_container : public Container {
+    Vector v;
+public:
+    Vector_container(int s): v(s) {}
+    Vector_container(std::initializer_list<double> lst): v(lst) {} //in carte nu e constructorul asta si functia go merge!!!
+    ~Vector_container() {}
+
+    double& operator[](int i) { return v[i];}
+    int size() const {return v.size();}
+
+};
+
+
+class List_container : public Container {
+    list<double> l;
+public:
+    List_container() {}
+    List_container(std::initializer_list<double> lst): l(lst) {} //in carte nu e constructorul asta si functia go merge!!!
+    ~List_container() {}
+
+    double& operator[](int i) {
+        for(auto& x: l){
+            if(i==0)
+                return x;
+            --i;
+        }
+            throw out_of_range("List container");
+    }
+
+    int size() const {return l.size();}
+
+};
+
+
+void go(){
+    Vector_container vc{10,9,8,7,6,5,4,3,2,1,0};
+    use(vc);
+}
+
+
+void h(){
+    List_container lc{1,2,3,4,5,6,7,8,9};
+    use(lc);
 }
 
 
@@ -55,6 +127,8 @@ int main()
     //Vector v(6);
     //cout << "Hello world!" << v[0] << endl;
 
-    cout << read_and_sum(3);
+    //cout << read_and_sum(3);
+    go();
+    h();
     return 0;
 }
